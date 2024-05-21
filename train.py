6f11d91ab067c2d40fcd31cea77ea273eefd7913
fenghan0430 <fenghan0430@outlook.com>
2024-05-21 09:58:15 +0800

    两种损失函数的训练脚本

--- train.py
+++ train.py
@@ -17,23 +17,33 @@ set_gpu_memory_mode()
 batch_size = 256
 epochs     = 1000
 save_name = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+loss_type = "masked_mse"
 
 # 加载数据集
-feat = np.load("loaded_feat.npy")
-label = np.load("loaded_label.npy")
-feat = np.transpose(feat, (0, 2, 3, 1)) # 读取时出错，下次读取时需要修改代码
-label = [label[:, :, :14],label[:, :, 14:]] # 14是指有多少个类 len(unique_classes)
-print("加载特征和标签完成")
 
-# 分割数据集
-dataset = tf.data.Dataset.from_tensor_slices((feat, (label[0], label[1])))
-del feat, label
-dataset = dataset.shuffle(6000)
-val_dataset = dataset.take(1800)
-train_dataset = dataset.skip(1800)
-del dataset
+if loss_type == "mse":
+    train_dataset = tf.data.Dataset.from_tensor_slices((np.load("t1/train_feat.npy"), (np.load("t1/train_label0.npy"), np.load("t1/train_label1.npy"))))
+    val_dataset = tf.data.Dataset.from_tensor_slices((np.load("t1/val_feat.npy"), (np.load("t1/val_label0.npy"), np.load("t1/val_label1.npy"))))
+elif loss_type == "masked_mse":
+    train_feat = np.load("t1/train_feat.npy")
+    train_label0 = np.load("t1/train_label0.npy")
+    train_label1 = np.load("t1/train_label1.npy")
+
+    val_feat = np.load("t1/val_feat.npy")
+    val_label0 = np.load("t1/val_label0.npy")
+    val_label1 = np.load("t1/val_label1.npy")
+    
+    train_label1 = np.concatenate([train_label0, train_label1], axis=-1)
+    val_label1 = np.concatenate([val_label0, val_label1], axis=-1)
+    
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_feat, (train_label0, train_label1)))
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_feat, (val_label0, val_label1)))
+print("加载数据集完成")
 
 # 设置批次
+train_dataset = train_dataset.shuffle(1000)
+val_dataset = val_dataset.shuffle(1000)
+
 train_dataset = train_dataset.batch(batch_size)
 val_dataset = val_dataset.batch(batch_size)
 print("数据集设置完成")
@@ -43,7 +53,10 @@ print("数据集设置完成")
 log_dir = "logs/fit" + save_name
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-model = get_model()
+if loss_type == "mse":
+    model = get_model()
+elif loss_type == "masked_mse":
+    model = get_model(doa_objective=loss_type)
 
 model.fit(train_dataset, epochs=epochs, validation_data=val_dataset, callbacks=[tensorboard_callback])
 
